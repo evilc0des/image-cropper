1b826e4da9f56eea5f919117c7f930d523f756e3
rapt0rdk <dhrtklpsh82@gmail.com>
2018-10-28 04:48:06 +0530

    added rollup

--- example/src/index.js
+++ example/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import CropComponent from "./CropComponent";
+import CropComponent from "../src/CropComponent";
 
 import "./styles.css";
 
